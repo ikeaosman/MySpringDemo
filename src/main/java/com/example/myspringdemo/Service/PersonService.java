aaf431dbae8b10ad42d12dee0de51ffb9fb5ef9f
ikeaosman <ikhr0003@stud.kea.dk>
2023-03-27 09:23:46 +0200

    4

--- src/main/java/com/example/myspringdemo/Service/PersonService.java
+++ src/main/java/com/example/myspringdemo/Service/PersonService.java
@@ -2,7 +2,9 @@ package com.example.myspringdemo.Service;
 
 import com.example.myspringdemo.Model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,8 +14,12 @@ public class PersonService { //Jeg har kopiet det fra PersonRepo klassen og skre
     @Autowired
     JdbcTemplate template;
 
-    public List<Person> fetchALl(){
+    public List<Person> fetchALl(){ //Den har fat i database og man skal lave en sql string her
+        String sql = "SELECT * FROM person";
+        RowMapper<Person> rowMapper = new BeanPropertyRowMapper<>(Person.class); // lister af objekter, af alle række i databasen
+
         //Her kommer vores sql kode til at stå
+
         return null;
     }
     public void addPerson(Person p){
