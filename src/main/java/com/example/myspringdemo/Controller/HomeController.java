a8ee7b78b9471f0bc8e71643bd9c2a931fbf05a3
ikeaosman <ikhr0003@stud.kea.dk>
2023-03-27 09:28:52 +0200

    6

--- src/main/java/com/example/myspringdemo/Controller/HomeController.java
+++ src/main/java/com/example/myspringdemo/Controller/HomeController.java
@@ -1,12 +1,21 @@
 package com.example.myspringdemo.Controller;
 
+import com.example.myspringdemo.Model.Person;
+import com.example.myspringdemo.Service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
+    @Autowired
+    PersonService personService;
+
     @GetMapping("/")
     public String index(){
+        List<Person> personList = personService.fetchALl();
         return "home/index";
     }
 }
