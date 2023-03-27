61c4b0e37ccb7a3fce808b1bc3d86c6dce902f3b
ikeaosman <ikhr0003@stud.kea.dk>
2023-03-27 09:14:53 +0200

    2

---
+++ src/main/java/com/example/myspringdemo/Resopository/PersonRepo.java
@@ -0,0 +1,27 @@
+package com.example.myspringdemo.Resopository;
+
+import com.example.myspringdemo.Model.Person;
+
+import java.util.List;
+
+public class PersonRepo {
+    //
+    //De 5 metoder. fetch people, addPerson, deleteperson, update person
+
+    public List<Person> fetchALl(){
+        //Her kommer vores sql kode til at stå
+        return null;
+    }
+    public void addPerson(Person p){
+
+    }
+    public Person findPersonById(int id){
+        return null;
+    }
+    public Boolean deletePerson(int id){
+     return null;
+    }
+    public void updatePerson(int id, Person p){
+
+    }
+}
