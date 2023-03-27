b47312ef6bdccb106f0fbb78f6734bd42fae77f6
ikeaosman <ikhr0003@stud.kea.dk>
2023-03-27 09:05:47 +0200

    1

---
+++ src/main/java/com/example/myspringdemo/Controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.myspringdemo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String index(){
+        return "home/index";
+    }
+}
