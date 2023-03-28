2d412dcd59444a51b1e3e0835aa28d0a43cef390
ikeaosman <ikhr0003@stud.kea.dk>
2023-03-28 13:50:24 +0200

    10

--- src/main/java/com/example/myspringdemo/Controller/HomeController.java
+++ src/main/java/com/example/myspringdemo/Controller/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -17,19 +18,49 @@ public class HomeController {
     PersonService personService;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model) {
         List<Person> personList = personService.fetchAll();
         model.addAttribute("persons", personList);
         return "home/index";
     }
+
     @GetMapping("/create")
-    public String create(){
+    public String create() {
         return "home/create";
     }
+
     @PostMapping("/createNew")
-    public String createNew(@ModelAttribute Person person){
+    public String createNew(@ModelAttribute Person person) {
         personService.addPerson(person);
         return "redirect:/";
     }
 
+    @GetMapping("/viewOne/{id}")
+    public String viewOne(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", personService.findPersonById(id));
+        return "home/viewOne";
+
+    }
+
+    @GetMapping("/deleteOne/{id}")
+    public String deleteOne(@PathVariable("id") int id) {
+        boolean deleted = personService.deletePerson(id);
+        if (deleted) {
+            return "redirect:/";
+        } else {
+            return "redirect:/";
+        }
+    }
+
+    @GetMapping("/updateOne/{id}")
+    public String updateOne(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", personService.findPersonById(id));
+        return "home/updateOne";
+
+    }
+    @PostMapping("/updatePerson")
+    public String updatePerson(@ModelAttribute Person person){
+        personService.updatePerson(person.getId(), person);
+        return "redirect:/";
+    }
 }
